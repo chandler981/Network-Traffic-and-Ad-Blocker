ff260a4d4605426800e2f3f33e7f2b7fbe106d05
Chandler Ward <chandlerward18@gmail.com>
2025-06-26 21:15:12 -0400

    Dependencies set and added
    This commit contains the project structure, which uses
    Spring Boot for handling the web server
    Lombok used for annotations to reduce boilerplate
    Littleproxy for intercepting, inspecting, and blocking requests
    Thymeleaf for creating dynamic HTML pages
    Jackson Databind for logging and serializing blocked request data

---
+++ networktracking/src/main/java/com/networktracking/networktracking/NetworktrackingApplication.java
@@ -0,0 +1,13 @@
+package com.networktracking.networktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworktrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworktrackingApplication.class, args);
+	}
+
+}
---
+++ networktracking/src/test/java/com/networktracking/networktracking/NetworktrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networktracking.networktracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworktrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
